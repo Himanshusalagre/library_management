77f3c6c8dcebcc4ba4addc20c327b65c4c5ed465
Himanshusalagre <himanshu.salagre@somaiya.edu>
2021-05-05 13:02:41 +0530

    first commit

---
+++ hooks/footer-extras.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Version</b> 1.1.0
+    </div>
+    <strong>&nbsp;Made By <a href=""> Himanshu & Umang</a>.</strong>
+  </footer>
